b9ed10849e17dbc64daa028ebc516274f22e4610
Osmani Hernandez <osmanihernandezperez@gmail.com>
2024-03-21 18:28:49 -0400

    Handle form submission

--- app/layaways/new/page.tsx
+++ app/layaways/new/page.tsx
@@ -1,16 +1,32 @@
 "use client";
 
 import { Button, TextArea, TextField } from "@radix-ui/themes";
-import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface layawayForm {
+  customerName: string;
+  customerPhone: string;
+  description: string;
+}
 
 const NewLayawayPage = () => {
+  const router = useRouter();
+  const { register, handleSubmit } = useForm<layawayForm>();
+
+  const onSubmit: SubmitHandler<layawayForm> = async (data) => {
+    await axios.post("/api/layaways", data);
+    router.push("/layaways");
+  };
+
   return (
-    <div className="max-w-xl space-y-3">
-      <TextField.Input placeholder="Customer's name" />
-      <TextField.Input placeholder="Customer's phone" />
-      <TextArea placeholder="Add a description" />
+    <form className="max-w-xl space-y-3" onSubmit={handleSubmit(onSubmit)}>
+      <TextField.Input placeholder="Full Name" {...register("customerName")} />
+      <TextField.Input placeholder="Phone" {...register("customerPhone")} />
+      <TextArea placeholder="Add a description" {...register("description")} />
       <Button>Submit new layaway</Button>
-    </div>
+    </form>
   );
 };
 
