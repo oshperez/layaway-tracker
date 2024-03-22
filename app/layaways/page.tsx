5e8d632ed45321baf2343dc2114b4ecf6f3bfb88
Osmani Hernandez <osmanihernandezperez@gmail.com>
2024-03-22 14:53:41 -0400

    Show the layaways

--- app/layaways/page.tsx
+++ app/layaways/page.tsx
@@ -1,12 +1,49 @@
 import React from "react";
-import { Button } from "@radix-ui/themes";
+import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
+import prisma from "@/prisma/client";
 
-const Layaways = () => {
+const Layaways = async () => {
+  const layaways = await prisma.layaway.findMany();
   return (
-    <Button>
-      <Link href="/layaways/new">New layaway</Link>
-    </Button>
+    <>
+      <div className="mb-5">
+        <Button>
+          <Link href="/layaways/new">New layaway</Link>
+        </Button>
+      </div>
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.RowHeaderCell>Name</Table.RowHeaderCell>
+            <Table.RowHeaderCell className="hidden lg:table-cell">
+              Description
+            </Table.RowHeaderCell>
+            <Table.RowHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.RowHeaderCell>
+            <Table.RowHeaderCell>Created</Table.RowHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {layaways.map((layaway) => (
+            <Table.Row key={layaway.id}>
+              <Table.Cell>
+                {layaway.customerName}
+                <div className="block md:hidden">{layaway.status}</div>
+              </Table.Cell>
+              <Table.Cell className="hidden lg:table-cell">
+                {layaway.description}
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {layaway.status}
+              </Table.Cell>
+              <Table.Cell>{layaway.createdAt.toDateString()}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </>
   );
 };
 
