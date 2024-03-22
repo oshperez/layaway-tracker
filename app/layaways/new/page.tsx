86eeda6ac1fc7905a062de0881cd2705cf26f974
Osmani Hernandez <osmanihernandezperez@gmail.com>
2024-03-22 09:28:06 -0400

    Handle errors

--- app/layaways/new/page.tsx
+++ app/layaways/new/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextArea, TextField } from "@radix-ui/themes";
 import { SubmitHandler, useForm } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 interface layawayForm {
   customerName: string;
@@ -12,21 +13,39 @@ interface layawayForm {
 }
 
 const NewLayawayPage = () => {
+  const [error, setError] = useState("");
   const router = useRouter();
   const { register, handleSubmit } = useForm<layawayForm>();
 
   const onSubmit: SubmitHandler<layawayForm> = async (data) => {
-    await axios.post("/api/layaways", data);
-    router.push("/layaways");
+    try {
+      await axios.post("/api/layaways", data);
+      router.push("/layaways");
+    } catch (error) {
+      setError("An unexpected error has occurred.");
+    }
   };
 
   return (
-    <form className="max-w-xl space-y-3" onSubmit={handleSubmit(onSubmit)}>
-      <TextField.Input placeholder="Full Name" {...register("customerName")} />
-      <TextField.Input placeholder="Phone" {...register("customerPhone")} />
-      <TextArea placeholder="Add a description" {...register("description")} />
-      <Button>Submit new layaway</Button>
-    </form>
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form className="space-y-3" onSubmit={handleSubmit(onSubmit)}>
+        <TextField.Input
+          placeholder="Full Name"
+          {...register("customerName")}
+        />
+        <TextField.Input placeholder="Phone" {...register("customerPhone")} />
+        <TextArea
+          placeholder="Add a description"
+          {...register("description")}
+        />
+        <Button>Submit new layaway</Button>
+      </form>
+    </div>
   );
 };
 
