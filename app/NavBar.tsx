dd6437c9476a42acb4db5ede4325a3c6a8834715
Osmani Hernandez <osmanihernandezperez@gmail.com>
2024-03-30 15:36:03 -0400

    Refactpr the nav bar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -9,70 +9,90 @@ import { usePathname } from "next/navigation";
 import { RiArchiveDrawerFill } from "react-icons/ri";
 
 const NavBar = () => {
-  const currentPath = usePathname();
-  const { status, data: session } = useSession();
-
-  const links = [
-    { label: "Dashboard", href: "/" },
-    { label: "Layaways", href: "/layaways" },
-  ];
-
   return (
     <nav className="border-b mb-5 px-5 py-4">
       <Container>
         <Flex justify="between">
           <Flex align="center" gap="4">
-            <Link href="/">
-              <RiArchiveDrawerFill />
-            </Link>
-            <ul className="flex space-x-6">
-              {links.map((link) => (
-                <li key={link.href}>
-                  <Link
-                    className={classnames({
-                      "text-zinc-900": link.href === currentPath,
-                      "text-zinc-500": link.href !== currentPath,
-                      "hover:text-zinc-800 transition-colors": true,
-                    })}
-                    href={link.href}
-                  >
-                    {link.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <NavLinks />
           </Flex>
-          <Box>
-            {status === "unauthenticated" && (
-              <Link href="/api/auth/signin">Sign in</Link>
-            )}
-            {status === "authenticated" && (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar
-                    src={session.user?.image!}
-                    fallback=":("
-                    size="2"
-                    radius="full"
-                    className="cursor-pointer"
-                  />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
-                  <DropdownMenu.Item>
-                    <PersonIcon />
-                    <Link href="/user/profile">Profile</Link>
-                  </DropdownMenu.Item>
-                  <DropdownMenu.Item>
-                    <ExitIcon />
-                    <Link href="/api/auth/signout">Sign out</Link>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            )}
-          </Box>
+          <AuthStatus />
         </Flex>
       </Container>
     </nav>
+  );
+};
+
+const NavLinks = () => {
+  const currentPath = usePathname();
+
+  const menuLinks = [
+    { label: "Dashboard", href: "/" },
+    { label: "Layaways", href: "/layaways" },
+  ];
+
+  return (
+    <>
+      <Link href="/">
+        <RiArchiveDrawerFill />
+      </Link>
+      <ul className="flex space-x-6">
+        {menuLinks.map((link) => (
+          <li key={link.href}>
+            <Link
+              className={classnames({
+                "nav-link": true,
+                "!text-zinc-900": link.href === currentPath,
+              })}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+
+  const menuLinks = [
+    { label: "Profile", href: "/user/profile", icon: <PersonIcon /> },
+    { label: "Sign out", href: "/api/auth/signout", icon: <ExitIcon /> },
+  ];
+
+  if (status === "loading") return null;
+  if (status === "unauthenticated")
+    return (
+      <Link href="/api/auth/signin" className="nav-link">
+        Sign in
+      </Link>
+    );
+
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            src={session!.user!.image!}
+            fallback=":)"
+            size="2"
+            radius="full"
+            className="cursor-pointer"
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          {menuLinks.map((link) => (
+            <DropdownMenu.Item key={link.label}>
+              {link.icon}
+              <Link href={link.href}>{link.label}</Link>
+            </DropdownMenu.Item>
+          ))}
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
   );
 };
 
