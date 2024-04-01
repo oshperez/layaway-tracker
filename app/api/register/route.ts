feee0cb33255fcdaf2a8afd2b40ae294bd6a9b0a
Osmani Hernandez <osmanihernandezperez@gmail.com>
2024-04-01 11:58:24 -0400

    Create register api

---
+++ app/api/register/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import bcrypt from "bcrypt";
+import prisma from "@/prisma/client";
+
+const schema = z.object({
+  name: z.string().min(2),
+  email: z.string().email(),
+  password: z.string().min(5),
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+
+  const validation = schema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
+  const user = await prisma.user.findUnique({ where: { email: body.email } });
+  if (user)
+    return NextResponse.json({ error: "User already exists" }, { status: 400 });
+
+  const hashedPassword = await bcrypt.hash(body.password, 10);
+
+  const newUser = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+      hashedPassword,
+    },
+  });
+
+  return NextResponse.json(
+    { name: newUser.name, email: newUser.email },
+    { status: 200 }
+  );
+}
