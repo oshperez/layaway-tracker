945e82bdcd83145f59e9ede63dc8b807e751b3a3
Osmani Hernandez <osmanihernandezperez@gmail.com>
2024-04-02 00:59:55 -0400

    Add register page

--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -5,3 +5,9 @@ export const layawaySchema = z.object({
   customerPhone: z.string().min(10, "Phone is required").max(255),
   description: z.string().min(1, "Description is required"),
 });
+
+export const userSchema = z.object({
+  name: z.string().min(5, "Name is required").max(255),
+  email: z.string().email("Email is required"),
+  password: z.string().min(5, "Password is required"),
+});
