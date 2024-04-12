4273c1aa9029af0600dcff8dae5e5cdba1a83b9f
Osmani Hernandez <osmanihernandezperez@gmail.com>
2024-04-11 23:52:37 -0400

    Build an API for updating customers

---
+++ app/api/customers/[id]/route.ts
@@ -0,0 +1,27 @@
+import { customerSchema } from "@/app/validationSchemas";
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { name, phone } = await request.json();
+  const validation = await customerSchema.safeParse({ name, phone });
+
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400 });
+
+  const customer = await prisma.customer.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!customer)
+    return NextResponse.json({ error: "Invalid Customer" }, { status: 404 });
+
+  const updatedCustomer = await prisma.customer.update({
+    where: { id: customer.id },
+    data: { name, phone },
+  });
+  return NextResponse.json(updatedCustomer, { status: 200 });
+}
