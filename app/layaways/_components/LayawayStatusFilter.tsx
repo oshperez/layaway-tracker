9d887ff6fd3b420bd0937dc20f2cdb0a0470441d
Osmani Hernandez <osmanihernandezperez@gmail.com>
2024-06-06 16:45:37 -0400

    Fix filtering bug

--- app/layaways/_components/LayawayStatusFilter.tsx
+++ app/layaways/_components/LayawayStatusFilter.tsx
@@ -2,7 +2,7 @@
 
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const statuses: { label: string; value?: Status }[] = [
   { label: "All" },
@@ -14,14 +14,21 @@ const statuses: { label: string; value?: Status }[] = [
 
 const LayawayStatusFilter = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
 
   const handleChange = (status?: Status) => {
-    const query = status ? `?status=${status}` : "";
+    const currentParams = Object.fromEntries(searchParams.entries());
+    const { status: _, ...otherParams } = currentParams;
+    const updatedParams = { ...(status && { status }), ...otherParams };
+
+    const params = new URLSearchParams(updatedParams);
+    const query = params.size ? `?${params.toString()}` : "";
     router.push("/layaways" + query);
   };
 
   return (
     <Select.Root
+      defaultValue={searchParams.get("status") || ""}
       onValueChange={(value: Status | "ALL") =>
         handleChange(value !== "ALL" ? value : undefined)
       }
