bc980ee853f8b2b8c01384ce96278386b47bed82
Osmani Hernandez <osmanihernandezperez@gmail.com>
2024-12-14 01:06:34 -0500

    Build the LatestLayaways component

---
+++ app/LatestLayaways.tsx
@@ -0,0 +1,52 @@
+import prisma from "@/prisma/client";
+import { Avatar, Card, Flex, Heading, Table } from "@radix-ui/themes";
+import Link from "next/link";
+import { LayawayStatusBadge } from "./components";
+
+const LatestLayaways = async () => {
+  const layaways = await prisma.layaway.findMany({
+    orderBy: { createdAt: "desc" },
+    take: 5,
+    include: {
+      customer: {
+        select: {
+          id: true,
+          name: true,
+        },
+      },
+    },
+  });
+  return (
+    <Card>
+      <Heading size="5" mb="5">
+        Latest layaways
+      </Heading>
+      <Table.Root>
+        <Table.Body>
+          {layaways.map((layaway) => (
+            <Table.Row key={layaway.id}>
+              <Table.Cell>
+                <Flex justify="between">
+                  <Flex direction="column" align="start" gap="2">
+                    <Link href={`layaways/${layaway.id}`}>{layaway.item}</Link>
+                    <LayawayStatusBadge status={layaway.status} />
+                  </Flex>
+                  <Link href={`customers/${layaway.customer.id}`}>
+                    <Avatar
+                      fallback={layaway.customer.name
+                        .slice(0, 2)
+                        .toLocaleUpperCase()}
+                      radius="large"
+                    />
+                  </Link>
+                </Flex>
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </Card>
+  );
+};
+
+export default LatestLayaways;
